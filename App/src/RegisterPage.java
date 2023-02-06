588bd1553568b4911a9fd15c47a6f76270f45048
Nik Krnjovsek <nik.krnjovsek@gmail.com>
2023-02-06 04:17:04 +0100

    fully updating login and register

--- App/src/RegisterPage.java
+++ App/src/RegisterPage.java
@@ -2,10 +2,12 @@ import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.event.ActionListener;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import java.sql.Connection;
@@ -13,7 +15,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class GUI implements ActionListener {
+public class RegisterPage implements ActionListener {
 
     JPanel panel;
     JFrame frame;
@@ -24,13 +26,14 @@ public class GUI implements ActionListener {
     private static JTextField userText;
     private static JTextField passwordText;
 
+    JButton registerButton;
     JButton loginButton;
 
-    public GUI() {
+    public RegisterPage() {
 
         panel = new JPanel();
         frame = new JFrame();
-        frame.setSize(350, 200);
+        frame.setSize(350, 240);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
@@ -52,14 +55,15 @@ public class GUI implements ActionListener {
         passwordText.setBounds(100, 60, 165, 25);
         panel.add(passwordText);
 
-        loginButton = new JButton(new AbstractAction("login") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                gettrust();
+        registerButton = new JButton("Register");
+        registerButton.addActionListener(this);
+        registerButton.setBounds(100, 100, 120, 25);
+        panel.add(registerButton);
 
-            }
-        });
-        loginButton.setBounds(100, 100, 120, 25);
+        loginButton = new JButton("Have an account?");
+        loginButton.addActionListener(this);
+        loginButton.setBounds(100, 150, 120, 20);
+        loginButton.setFont(new Font("Arial", Font.PLAIN, 8));
         panel.add(loginButton);
 
         frame.setVisible(true);
@@ -67,7 +71,7 @@ public class GUI implements ActionListener {
 
     public static void main(String[] args) throws Exception {
 
-        new GUI();
+        new RegisterPage();
     }
 
     public void gettrust() {
@@ -78,15 +82,9 @@ public class GUI implements ActionListener {
                             "jdbc:postgresql://ep-wild-darkness-767526.eu-central-1.aws.neon.tech/neondb?user=nik.krnjovsek&password=a1hjwRmFZeE0",
                             "nik.krnjovsek", "a1hjwRmFZeE0");
             Statement select = c.createStatement();
-            String sql = "SELECT login('" + userText.getText() + "', '" + passwordText.getText() + "')";
-            ResultSet rs = select.executeQuery(sql);
-            if (rs.next()) {
-                if (rs.getBoolean(1)) {
-                    System.out.print("logged in!");
-                } else {
-                    System.out.print("napacni podatki!");
-                }
-            }
+            String sql = "SELECT register('" + userText.getText() + "', '" + passwordText.getText() + "')";
+            select.executeQuery(sql);
+
         } catch (Exception e1) {
             e1.printStackTrace();
         }
@@ -94,6 +92,14 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == registerButton) {
+            gettrust();
+            LoginPage loginPage = new LoginPage();
+            frame.dispose();
+        }
+        if (e.getSource() == loginButton) {
+            LoginPage loginPage = new LoginPage();
+            frame.dispose();
+        }
     }
 }
